737efe29fd01eb64200dd26a921f9b74859f941b
luanheider <luanheider@hotmail.com>
2025-02-24 12:24:17 -0400

    adicionadas classes de security

---
+++ src/main/java/com/luanheider/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.luanheider.usuario.infrastructure.repository;
+
+import com.luanheider.aprendendospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
